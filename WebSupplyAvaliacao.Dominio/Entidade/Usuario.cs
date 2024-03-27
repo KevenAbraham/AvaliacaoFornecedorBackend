f713026f4b50e5730385c9b8bbc118e99236ae64
KevenAbraham <abraham.keven20@gmail.com>
2024-03-27 15:29:29 -0300

    feat: crud do usuário

--- WebSupplyAvaliacao.Dominio/Entidade/Usuario.cs
+++ WebSupplyAvaliacao.Dominio/Entidade/Usuario.cs
@@ -6,11 +6,20 @@ public class Usuario
 {
     [Key]
     public int ID { get; set; }
-    
+
+    [Required(ErrorMessage = "O nome já foi escolhido")]
     [StringLength(80)]
+    [Display(Name = "Nome:")]
     public string Nome { get; set; }
 
+    [EmailAddress(ErrorMessage = "Informe um email válido")]
     [StringLength(100)]
+    [Display(Name = "E-mail:")]
     public string Email { get; set; }
     public bool Status { get; set; }
+
+    public Usuario()
+    {
+        Status = true;
+    }
 }
---
+++ WebSupplyAvaliacao.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSupplyAvaliacao.Dados.Context;
+using WebSupplyAvaliacao.Dominio.Entidade;
+
+namespace WebSupplyAvaliacao.Web.Controllers;
+
+public class UsuarioController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public UsuarioController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpPost]
+    public IActionResult Cadastrar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Cadastrar(Usuario usuario)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Usuario.Add(usuario);
+            _context.SaveChanges();
+            return RedirectToAction("Confirmacao", "Usuario");
+        }
+        return View(usuario);
+    }
+
+    [HttpGet]
+    public IActionResult Confirmacao(Usuario usuario)
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Listar()
+    {
+        var usuarios = _context.Usuario.ToList();
+        return View(usuarios);
+    }
+
+    [HttpGet]
+    public IActionResult Editar(int id)
+    {
+        var usuario = _context.Usuario.SingleOrDefault(user => user.ID == id);
+
+        return View(usuario);
+    }
+
+    [HttpPost]
+    public IActionResult Editar(int id, [Bind("ID", "Nome", "Email", "Status")] Usuario usuario)
+    {
+        _context.Usuario.Update(usuario);
+        _context.SaveChanges();
+
+        return View(usuario);
+
+    }
+}
