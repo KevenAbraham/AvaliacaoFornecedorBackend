5f82b056ff1b98f0f0b206fc72b3ea8a8b55c2d4
KevenAbraham <abraham.keven20@gmail.com>
2024-05-09 13:57:53 -0300

    feat: criando a tela de indicadores de avaliação de fornecedor

--- WebSupplyAvaliacao.Web/Controllers/HomeController.cs
+++ WebSupplyAvaliacao.Web/Controllers/HomeController.cs
@@ -4,94 +4,93 @@ using System.Diagnostics;
 using WebSupplyAvaliacao.Dados.Context;
 using WebSupplyAvaliacao.Web.Models;
 
-namespace WebSupplyAvaliacao.Web.Controllers
+namespace WebSupplyAvaliacao.Web.Controllers;
+
+[Authorize]
+[ServiceFilter(typeof(Validacao))]
+public class HomeController : Controller
 {
-    [Authorize]
-    [ServiceFilter(typeof(Validacao))]
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+    private readonly AppDbContext _context;
+
+    public HomeController(ILogger<HomeController> logger, AppDbContext context)
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly AppDbContext _context;
+        _logger = logger;
+        _context = context;
+    }
 
-        public HomeController(ILogger<HomeController> logger, AppDbContext context)
+    public IActionResult Index() //Mesmo método já está documentado na Controller de Avaliação.
+    {
+        int qtdFornecedores = _context.Fornecedor.Count(); //quantidade total de fornecedores
+        ViewBag.QtdFornecedor = qtdFornecedores;
+
+        DateTime primeiroDiaMesAtual = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+        DateTime primeiroDiaProximoMes = primeiroDiaMesAtual.AddMonths(1);
+
+        int qtdFornecedoresMonth = _context.Fornecedor
+            .Count(f => f.Data >= primeiroDiaMesAtual && f.Data < primeiroDiaProximoMes); // fornecedores cadastrados neste mês
+
+        ViewBag.QtdFornecedoresUltimoMes = qtdFornecedoresMonth;
+
+        // Inicializa as contagens de estrelas fora do loop
+        int fornecedores5Estrelas = 0;
+        int fornecedores4Estrelas = 0;
+        int fornecedores3Estrelas = 0;
+        int fornecedores2Estrelas = 0;
+        int fornecedores1Estrela = 0;
+
+        foreach (var fornecedor in _context.Fornecedor.ToList())
         {
-            _logger = logger;
-            _context = context;
-        }
+            // Filtra as avaliações apenas para o fornecedor atual
+            var avaliacoesFornecedor = _context.Avaliar.Where(a => a.FornecedorId == fornecedor.ID).ToList();
 
-        public IActionResult Index() //Mesmo método já está documentado na Controller de Avaliação.
-        {
-            int qtdFornecedores = _context.Fornecedor.Count(); //quantidade total de fornecedores
-            ViewBag.QtdFornecedor = qtdFornecedores;
+            double media = 0;
+            double mediaRound = 0;
 
-            DateTime primeiroDiaMesAtual = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
-            DateTime primeiroDiaProximoMes = primeiroDiaMesAtual.AddMonths(1);
-
-            int qtdFornecedoresMonth = _context.Fornecedor
-                .Count(f => f.Data >= primeiroDiaMesAtual && f.Data < primeiroDiaProximoMes); // fornecedores cadastrados neste mês
-
-            ViewBag.QtdFornecedoresUltimoMes = qtdFornecedoresMonth;
-
-            // Inicializa as contagens de estrelas fora do loop
-            int fornecedores5Estrelas = 0;
-            int fornecedores4Estrelas = 0;
-            int fornecedores3Estrelas = 0;
-            int fornecedores2Estrelas = 0;
-            int fornecedores1Estrela = 0;
-
-            foreach (var fornecedor in _context.Fornecedor.ToList())
+            if (avaliacoesFornecedor.Any())
             {
-                // Filtra as avaliações apenas para o fornecedor atual
-                var avaliacoesFornecedor = _context.Avaliar.Where(a => a.FornecedorId == fornecedor.ID).ToList();
+                media = avaliacoesFornecedor.Average(a => a.Nota);
+                mediaRound = Math.Round(media, 2);
 
-                double media = 0;
-                double mediaRound = 0;
-
-                if (avaliacoesFornecedor.Any())
+                if (mediaRound >= 4.5)
                 {
-                    media = avaliacoesFornecedor.Average(a => a.Nota);
-                    mediaRound = Math.Round(media, 2);
-
-                    if (mediaRound >= 4.5)
-                    {
-                        fornecedores5Estrelas++;
-                    }
-                    else if (mediaRound >= 3.5)
-                    {
-                        fornecedores4Estrelas++;
-                    }
-                    else if (mediaRound >= 2.5)
-                    {
-                        fornecedores3Estrelas++;
-                    }
-                    else if (mediaRound >= 1.5)
-                    {
-                        fornecedores2Estrelas++;
-                    }
-                    else
-                    {
-                        fornecedores1Estrela++;
-                    }
+                    fornecedores5Estrelas++;
+                }
+                else if (mediaRound >= 3.5)
+                {
+                    fornecedores4Estrelas++;
+                }
+                else if (mediaRound >= 2.5)
+                {
+                    fornecedores3Estrelas++;
+                }
+                else if (mediaRound >= 1.5)
+                {
+                    fornecedores2Estrelas++;
+                }
+                else
+                {
+                    fornecedores1Estrela++;
                 }
             }
-
-            // Define as ViewBag com as contagens de estrelas
-            ViewBag.Forn1Estrela = fornecedores1Estrela;
-            ViewBag.Forn2Estrelas = fornecedores2Estrelas;
-            ViewBag.Forn3Estrelas = fornecedores3Estrelas;
-            ViewBag.Forn4Estrelas = fornecedores4Estrelas;
-            ViewBag.Forn5Estrelas = fornecedores5Estrelas;
-
-            return View();
         }
 
+        // Define as ViewBag com as contagens de estrelas
+        ViewBag.Forn1Estrela = fornecedores1Estrela;
+        ViewBag.Forn2Estrelas = fornecedores2Estrelas;
+        ViewBag.Forn3Estrelas = fornecedores3Estrelas;
+        ViewBag.Forn4Estrelas = fornecedores4Estrelas;
+        ViewBag.Forn5Estrelas = fornecedores5Estrelas;
+
+        return View();
+    }
 
 
-        [AllowAnonymous]
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
+    [AllowAnonymous]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ WebSupplyAvaliacao.Web/Controllers/IndicadorController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using WebSupplyAvaliacao.Dados.Context;
+using WebSupplyAvaliacao.Web.Models;
+
+namespace WebSupplyAvaliacao.Web.Controllers;
+
+[Authorize]
+[ServiceFilter(typeof(Validacao))]
+public class IndicadorController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public IndicadorController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var qp = _context.Avaliar.Where(x => x.ServicoAvaliadoId == 1).Count();
+        var qs = _context.Avaliar.Where(x => x.ServicoAvaliadoId == 2).Count();
+        var dt = _context.Avaliar.Where(x => x.ServicoAvaliadoId == 3).Count();
+
+        int qps1 = 0;
+        int qps2 = 0;
+        int qps3 = 0;
+        int qps4 = 0;
+        int qps5 = 0;
+
+        for (int i = 0; i < qp; i++)
+        {
+            var star = _context.Avaliar.Where(x => x.ServicoAvaliadoId == 1).Skip(i).Select(x => x.Nota).FirstOrDefault();
+
+            switch (star) 
+            {
+                case 1:
+                    qps1++;
+                    break;
+                case 2:
+                    qps2++;
+                    break;
+                case 3:
+                    qps3++;
+                    break;
+                case 4: 
+                    qps4++;
+                    break;
+                case 5:
+                    qps5++;
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid method");
+            }
+        }
+
+        ViewBag.qps1 = qps1;
+        ViewBag.qps2 = qps2;
+        ViewBag.qps3 = qps3;
+        ViewBag.qps4 = qps4;
+        ViewBag.qps5 = qps5;
+
+        int qss1 = 0;
+        int qss2 = 0;
+        int qss3 = 0;
+        int qss4 = 0;
+        int qss5 = 0;
+
+        for (int i = 0; i < qs; i++)
+        {
+            var star = _context.Avaliar.Where(x => x.ServicoAvaliadoId == 2).Skip(i).Select(x => x.Nota).FirstOrDefault();
+
+            switch (star)
+            {
+                case 1:
+                    qss1++;
+                    break;
+                case 2:
+                    qss2++;
+                    break;
+                case 3:
+                    qss3++;
+                    break;
+                case 4:
+                    qss4++;
+                    break;
+                case 5:
+                    qss5++;
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid method");
+            }
+        }
+
+        ViewBag.qss1 = qss1;
+        ViewBag.qss2 = qss2;
+        ViewBag.qss3 = qss3;
+        ViewBag.qss4 = qss4;
+        ViewBag.qss5 = qss5;
+
+        int dts1 = 0;
+        int dts2 = 0;
+        int dts3 = 0;
+        int dts4 = 0;
+        int dts5 = 0;
+
+        for (int i = 0; i < dt; i++)
+        {
+            var star = _context.Avaliar.Where(x => x.ServicoAvaliadoId == 3).Skip(i).Select(x => x.Nota).FirstOrDefault();
+
+            switch (star)
+            {
+                case 1:
+                    dts1++;
+                    break;
+                case 2:
+                    dts2++;
+                    break;
+                case 3:
+                    dts3++;
+                    break;
+                case 4:
+                    dts4++;
+                    break;
+                case 5:
+                    dts5++;
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid method");
+            }
+        }
+
+        ViewBag.dts1 = dts1;
+        ViewBag.dts2 = dts2;
+        ViewBag.dts3 = dts3;
+        ViewBag.dts4 = dts4;
+        ViewBag.dts5 = dts5;
+
+        return View();
+    }
+}
